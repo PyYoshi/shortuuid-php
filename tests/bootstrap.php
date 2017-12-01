963b81d427c87c6b99309fcd53c9ae2ae76ed128
Jacek Kobus <jacek.kobus@audioteka.com>
2017-12-01 13:09:23 +0100

    added composer autoloader (fixes #4)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
+
 error_reporting(E_ALL);
 
-require dirname(__DIR__) . "/vendor/autoload.php";
-require_once dirname(__DIR__) . '/src/ShortUUID/Autoloader.php';
-\ShortUUID\Autoloader::register();
+require __DIR__ . '/../vendor/autoload.php';
